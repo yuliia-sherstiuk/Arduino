3bd6f094b5b898418d81a4e56e750396194fd151
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-04-07 18:23:42 +0200

    Ajout des jobs 1-5

---
+++ job2/sketch_apr7i/sketch_apr7i.ino
@@ -0,0 +1,24 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(11, HIGH);
+  delay(1000); // Delay a little bit to improve simulation performance
+  digitalWrite(11, LOW);
+  delay(1000); // Delay a little bit to improve simulation performance
+  digitalWrite(11, HIGH);
+  delay(1000); // Delay a little bit to improve simulation performance
+  digitalWrite(11, LOW);
+  delay(1000); // Delay a little bit to improve simulation performance
+  digitalWrite(11, HIGH);
+  delay(1000); // Delay a little bit to improve simulation performance
+  digitalWrite(11, LOW);
+  delay(1000); // Delay a little bit to improve simulation performance
+  
+}
+
